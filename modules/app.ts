d75ff9dfa9c46cbbc9e7ed8c83d16f72aa392df7
Musa Musa <musabrillz@gmail.com>
2020-03-28 00:57:53 +0100

    exporting modules

---
+++ modules/app.ts
@@ -0,0 +1,14 @@
+// Importing all
+// import * as math from './utils'
+// math.pi;
+// math.n
+
+//Being specific about import
+/* import {pi as PI} from './utils'
+console.log(PI); */
+
+// using default export
+// import MathModule from './utils'
+
+//Non-default export
+import {StringModule} from './utils'
---
+++ modules/utils.ts
@@ -0,0 +1,14 @@
+export const pi = 3.14
+export const n = 1;
+
+export default class MathModule {
+    calculate(n, m) {
+        return n + m
+    }
+}
+
+export class StringModule {
+    calculate(n, m) {
+        return n + m
+    }
+}
