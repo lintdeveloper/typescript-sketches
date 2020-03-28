1986803ca6fb6cb071aa5c08e4b15632d456c0e8
Musa Musa <musabrillz@gmail.com>
2020-03-28 04:22:01 +0100

    working with js objects

---
+++ non-primitive-data/objects.ts
@@ -0,0 +1,15 @@
+//  const person = {
+//     age: 43
+//  };
+//  person.age = 56;
+
+const users: {name: string, age: number}[] = [
+    {
+        name: "Bisi",
+        age: 34
+    },
+    {
+        name: "Fred",
+        age: 4
+    }
+]
