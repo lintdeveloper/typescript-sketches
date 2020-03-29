cea69ff5c4aff57540261f9fa05843cbf200a7a8
Musa Musa <musabrillz@gmail.com>
2020-03-29 15:40:07 +0100

    linting tslint code

--- typing-transpilation-debugging/tsc-webpack/app.ts
+++ typing-transpilation-debugging/tsc-webpack/app.ts
@@ -1,6 +1,8 @@
+// /* tslint:disable */ --> not advisable
 import Hero from './hero';
 
-let batman = new Hero('batman');
+const batman = new Hero('batman');
 batman.age = 33;
 batman.addPower("money");
+// /* tslint:disable:no-console */
 console.log(batman.listPowers());
