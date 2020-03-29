6118e3d9a7c34d8fe7ca07cd600772bedbadf6be
Musa Musa <musabrillz@gmail.com>
2020-03-29 13:42:28 +0100

    working with generics

---
+++ generics/generics.ts
@@ -0,0 +1,32 @@
+/*
+function pickNumber(numbers: number[]): number {
+    const randomIndex = Math.floor(Math.random() * numbers.length);
+    return numbers[randomIndex];
+}
+
+const numbers = [...Array(13).keys()];
+const pickedNumber = pickNumber(numbers);
+console.log(pickedNumber);
+
+function pickSuit(suits: string[]): string {
+  const randomIndex = Math.floor(Math.random() * suits.length);
+  return suits[randomIndex];
+}
+
+const suits = ["diamonds", "clubs", "hearts", "spades"];
+const pickedSuit = pickSuit(suits);
+console.log(pickedSuit); */
+
+// generic fn --> handles suit picking and number picking at without 
+//      writing several functions
+
+function picker<T>(args: T[]): T {
+    const randomIndex = Math.floor(Math.random() * args.length);
+    return args[randomIndex];
+}
+
+const suits = ['diamonds', 'clubs', 'hearts', 'spades'];
+const numbers = [...Array(13).keys()];
+const pickedNumber: number = picker(numbers);
+const pickedSuit: string = picker(suits);
+console.log(`Your card is: ${pickedNumber} ${pickedSuit}`);
